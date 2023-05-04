7c881e1b1681a9a9b0b38d9b7ad72318dfd79f91
William Combee <william.combee@yahoo.com>
2023-05-03 21:05:46 -0500

    added Candidate model

---
+++ models/Candidate.js
@@ -0,0 +1,58 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Candidate extends Model {}
+
+
+Candidate.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      vision: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      education: {
+        type: DataTypes.STRING,
+      },
+      work_experience: {
+        type: DataTypes.STRING,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+            isEmail: true,
+        },
+      },
+      votes: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'user',
+          key: 'id',
+        },
+      }
+    },
+    {
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'candidate',
+    }
+  );
+  
+  module.exports = Candidate;
