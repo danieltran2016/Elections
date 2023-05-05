f9f75a7002b48113a58a8d48b252d38ad5a99851
xybai0103 <xueyin0103@gmail.com>
2023-05-04 20:13:22 -0500

    move newCandidate.handlebars out of the layouts folder

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -19,6 +19,26 @@ router.get('/', async (req, res) => {
   }
 });
 
+// Use withAuth middleware to prevent access to run for the election
+router.get('/newCandidate', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Project }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('profile', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 router.get('/login', (req, res) => {
   if (req.session.logged_in) {
     res.redirect('/');
