049e2c294bf2ac2781c5861e4bb38286d82e65fb
xybai0103 <xueyin0103@gmail.com>
2023-05-07 13:15:58 -0500

    finish editing chart.js so that it gives real-time display of vote results

--- public/js/chart.js
+++ public/js/chart.js
@@ -13,8 +13,8 @@ data: {
     datasets: [{
     label: 'Votes',
     data: candidateVotes,
-    backgroundColor: 'rgba(255, 99, 132, 0.2)',
-    borderColor: 'rgba(255, 99, 132, 1)',
+    backgroundColor: 'rgba(54, 162, 235, 0.5)',
+    borderColor: 'rgba(54, 162, 235, 1)',
     borderWidth: 1
     }]
 },
